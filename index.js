e3a4eff7046964114c10bdbb02374abc0f8edb37
Katie Dear <Katie@Katies-MacBook-Air.local>
2020-02-28 09:02:16 +0000

    changes made to index.js and creation of index.html

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 var http = require("http");
-const PORT = process.env.PORT || 3000;
+fs = require('fs');
+const PORT = process.env.PORT || 8080;
 
 http.createServer(function (request, response) {
   // Send the HTTP header
   // HTTP Status: 200 : OK
-  // Content Type: text/plain
-  response.writeHead(200, {'Content-Type': 'text/plain'});
+  // Content Type: text/html
 
-  // Send the response body as "Hello World"
-  response.end('Hello World\n');
+  // write to our server. Set configuration for the response
+  response.writeHead(200, {
+    'Content-Type': 'text/html',
+    'Access-Control-Allow-Origin' : '*'
+  });
+  var readStream = fs.createReadStream(__dirname + '/index.html');
+  // send a message
+  readStream.pipe(response);
 }).listen(PORT);
 
 // Console will print the message
